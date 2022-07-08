de60816a97108e6252643d6aacf5be44282fd216
dheepak4580 <dheeps.4580@gmail.com>
2022-07-08 11:34:47 +0530

    Country code added

--- form.js
+++ form.js
@@ -1,10 +1,13 @@
 var input = document.getElementById('phone');
 var form = document.getElementById('form');
+var country = document.getElementById('Cc');
+var India = country.childNodes[1];
+var otherCountries = country.childNodes[3];
 
-var elem 				= document.createElement('div');
- 	elem.id  			='notify';
- 	elem.style.diplay	='none';
-    form.appendChild(elem);
+// var elem 				= document.createElement('div');
+//  	elem.id  			='notify';
+//  	elem.style.diplay	='none';
+//     form.appendChild(elem);
 
 /*input.addEventListener('input',function(event){
 	
@@ -16,6 +19,10 @@ var elem 				= document.createElement('div');
 	}
 });
 */
-input.oninvalid = function(event) {
+input.oninvalid = function(event){
     event.target.setCustomValidity('Phone number should start with a number from 6 to 9 and it should consist of 10 numbers in total');
+	console.log(event);
 }
+
+India.value= "+" + India.value;
+otherCountries.value = "+" + otherCountries.value;
